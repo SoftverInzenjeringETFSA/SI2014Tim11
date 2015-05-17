6a59bd922455cbda085de6da86de7b9f0e41c058
zloco <aploco1@etf.unsa.ba>
2015-05-17 15:00:25 +0200

    Dodana login forma i folder icons

---
+++ Projekat-Tim11/src/main/java/ba/unsa/etf/si/Tim11/Projekat_Tim11/GUI/Login.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.si.Tim11.Projekat_Tim11.GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Login {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Login");
+		ImageIcon img = new ImageIcon("icons/login_icon.png");
+		frame.setIconImage(img.getImage());
+	}
+
+}
