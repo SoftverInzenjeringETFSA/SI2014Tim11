27b499c60355876b77aa7db0e7fc47e973610727
Zannaa <zana_14t@hotmail.com>
2015-05-23 17:44:11 +0200

    Izmjene

--- Projekat-Tim11/src/test/java/ba/unsa/etf/si/Tim11/Projekat_Tim11/Klase/TestPlata.java
+++ Projekat-Tim11/src/test/java/ba/unsa/etf/si/Tim11/Projekat_Tim11/Klase/TestPlata.java
@@ -1,7 +1,10 @@
 package ba.unsa.etf.si.Tim11.Projekat_Tim11.Klase;
 
+import java.util.Date;
+
 import junit.framework.TestCase;
-import junit.framework.JUnit4TestAdapter;
+
+
 
 import org.junit.Test;
 import org.junit.Ignore;
@@ -139,14 +142,67 @@ public class TestPlata extends TestCase {
 
 	@Test (expected=Exception.class) public void testSetGodisnjiOdmor() {
 		 try {
-				Plata p=new Plata();
-				p.setGodisnjiOdmor(31);
-				fail("Nije bacen izuzetak");
+			 Firma _f = new Firma();
+			 Zaposlenik _z= new Zaposlenik(_f,"Adnan","Muslija","1502994190023","Džamijska 4","+38761508633",new Date(),"zaposlenik",10.0,1.0,2.7,300.0);				
+			 Plata p=new Plata(_z,new Date(),14.5, 1.0, 0.7,300,120,2,55,4,0);	
+			 p.setGodisnjiOdmor(31);
+				
 		        }
 		        catch(Exception e) {
-		        	assertTrue(true);
-		        }
+		        	System.err.println(e.getMessage());	
+		        } 
 		
+	
+	
+} 
+ public void testizracunajMinuliRad()
+	
+	{   try{
+		Plata p=new Plata();
+		p.setGodineStaza(1);
+		assertEquals(0.6, p.izracunajMinuliRad(100));
 	}
+	catch(Exception e)
+	{
+		 System.err.println(e.getMessage());
+	}
+	}
+ public void testizracunajDohodak(){
+	 try{
+			Plata p=new Plata();
+			p.setGodineStaza(100);
+			p.setOsnovica(1);
+			p.setKoeficijent(1);
+		    p.izracunajMinuliRad(100);
+		   double dohodak= p.izracunajDohodak();
+		    assertEquals(1.6, dohodak);
+		}
+		catch(Exception e)
+		{
+			 System.err.println(e.getMessage());
+		}
+	 
+	 
+ }
+ 
+ public void testizracunajStopuPoreza()
+ {
+	 try{
+			Plata p=new Plata();
+			
+		    assertEquals((100./31), p.izracunajStopuPoreza());
+		}
+		catch(Exception e)
+		{
+			 System.err.println(e.getMessage());
+		}
+	 
+	 
+	 
+	 
+	 
+ }
+ 
 
+ 
 }
