904300fa80b43fb1b97fc6c732967c67bac783c2
Irma12 <isolakovic1@etf.unsa.ba>
2015-05-29 22:55:04 +0200

    Izmjenjen regex

--- Projekat-Tim11/src/main/java/ba/unsa/etf/si/Tim11/Projekat_Tim11/Klase/Firma.java
+++ Projekat-Tim11/src/main/java/ba/unsa/etf/si/Tim11/Projekat_Tim11/Klase/Firma.java
@@ -62,14 +62,15 @@ public class Firma implements Serializable {
 	}
 
 	public void setIme(String ime) throws Exception {
-		 Pattern pattern = Pattern.compile("[a-zA-Z0-9\\s\\.]{3,}"); //mogu se unijeti mala, velika slova, brojevi, i tacka
-		    if (!pattern.matcher(ime).matches()) 
+		 Pattern pattern = Pattern.compile("[a-zA-ZĐđŠšČčĆćŽž0-9\\s\\.]{3,}"); //mogu se unijeti mala, velika slova, brojevi, i tacka
+
+			if (!pattern.matcher(ime).matches()) 
 		    	throw new Exception("Ime mora imati najmanje 3 karaktera(dozvoljena velika, mala slova brojevi tacka i razmak)");
 		
 		this.ime = ime;
 	}
 	public void setSjediste(String sjediste) throws Exception{
-		 Pattern pattern = Pattern.compile("[a-zA-Z0-9\\s\\.]{3,}"); //mogu se unijeti mala, velika slova, brojevi, i tacka
+		 Pattern pattern = Pattern.compile("[a-zA-ZĐđŠšČčĆćŽž0-9\\s\\.]{3,}"); //mogu se unijeti mala, velika slova, brojevi, i tacka
 if (!pattern.matcher(sjediste).matches()) 
 		    	throw new Exception("Adresa mora imati najmanje 3 karaktera(dozvoljena velika, mala slova brojevi tacka i razmak)");
 		
